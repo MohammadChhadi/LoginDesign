6ed8be239b85886c47ab216a053c9451e47b119e
MohamadChhadi <105631861+MohamadChhadi@users.noreply.github.com>
2022-05-24 15:29:05 +0300

    Merge branch 'main' of https://github.com/integrateddigitalsystems/AndroidTaskProject
    # Conflicts:
    #	.gitignore

---
+++ app/src/main/java/com/ids/logindesign/utils/AppHelper.kt
@@ -0,0 +1,61 @@
+package com.ids.logindesign.utils
+
+
+import android.content.Context
+import android.content.res.Configuration
+import android.os.Build
+import com.ids.logindesign.Controller.MyApplication
+import utils.AppConstants
+import utils.LocaleUtils
+import java.util.*
+
+
+
+class AppHelper {
+    companion object {
+        fun changeLanguage(context: Context, language: String) {
+
+            when (language) {
+                AppConstants.LANG_ARABIC -> Locale.setDefault(Locale("ar"))
+                AppConstants.LANG_ENGLISH -> Locale.setDefault(Locale.ENGLISH)
+                "0" -> {
+                    Locale.setDefault(Locale.ENGLISH)
+                }
+            }
+
+            val configuration = Configuration()
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+                configuration.setLocale(Locale.getDefault())
+                configuration.setLayoutDirection(Locale.getDefault())
+
+            } else
+                configuration.locale = Locale.getDefault()
+
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+                context.createConfigurationContext(configuration);
+            } else {
+                context.resources.updateConfiguration(
+                    configuration,
+                    context.resources.displayMetrics
+                )
+            }
+            MyApplication.languageCode = language
+
+
+        }
+
+
+        fun setLocal(context: Context) {
+
+            if (MyApplication.languageCode == AppConstants.LANG_ENGLISH) {
+                LocaleUtils.setLocale(Locale("en"))
+            } else if (MyApplication.languageCode == AppConstants.LANG_ARABIC) {
+                LocaleUtils.setLocale(Locale("ar"))
+            }
+
+        }
+
+    }
+
+}
